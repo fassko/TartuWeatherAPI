f0aa81364b1f7d95fadd99940b25b77443451784
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-05-26 21:48:58 +0200

    Set image path

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ public func routes(_ router: Router) throws {
   
   let weatherDataController = WeatherDataController()
   router.get("/", use: weatherDataController.live)
-  router.get("/liveImage", use: weatherDataController.liveImage)
+  router.get("/liveImage.jpg", use: weatherDataController.liveImage)
   
   let historyDataController = HistoryDataController()
   router.get("/history", use: historyDataController.history)
