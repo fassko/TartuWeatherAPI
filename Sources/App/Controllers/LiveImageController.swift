b900fde9e511036bc60361ec5ac6af81e98b081d
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-05-27 10:20:22 +0200

    Image without future

--- Sources/App/Controllers/LiveImageController.swift
+++ Sources/App/Controllers/LiveImageController.swift
@@ -9,25 +9,15 @@ import Vapor
 
 final class LiveImageController {
   
-  func large(_ req: Request) throws -> Future<Response> {
+  func large(_ req: Request) throws -> Response {
     return try liveImage(req, "http://meteo.physic.ut.ee/webcam/uus/suur.jpg")
   }
   
-  func small(_ req: Request) throws -> Future<Response> {
+  func small(_ req: Request) throws -> Response {
     return try liveImage(req, "http://meteo.physic.ut.ee/webcam/uus/pisike.jpg")
   }
   
-  func liveImage(_ req: Request, _ url: String) throws -> Future<Response> {
-    
-    let client = try req.sharedContainer.client()
-    
-    return client.get(url)
-    
-//    let client = try req.make(Client.self)
-//    let headers = HTTPHeaders.init([("Content-Type", "image/jpeg")])
-//    return client.get(url, headers: headers, beforeSend: { request in
-    
-//    })
-      //.get(url)
+  func liveImage(_ req: Request, _ url: String) throws -> Response {
+    return try req.sharedContainer.client().get(url)
   }
 }
